374b4df856c3d679dfbd05d8a0bf704e45ac61b8
yajamon <yajamon.tatsuki@gmail.com>
2016-12-05 12:13:08 +0900

    fix format

--- Model/AnalyserWrapper.ts
+++ Model/AnalyserWrapper.ts
@@ -4,20 +4,20 @@
 class AnalyserWrapper {
     canvas = document.createElement("canvas");
     context = this.canvas.getContext("2d");
-    latestRequestAnimationId:number = null;
+    latestRequestAnimationId: number = null;
 
-    bufferLength:number;
-    dataArray:Uint8Array;
-    constructor(public analyser:AnalyserNode) {
+    bufferLength: number;
+    dataArray: Uint8Array;
+    constructor(public analyser: AnalyserNode) {
         this.analyser.fftSize = 2048;
         this.bufferLength = analyser.frequencyBinCount;
         this.dataArray = new Uint8Array(this.bufferLength);
     }
 
-    draw(){
+    draw() {
         this.latestRequestAnimationId = requestAnimationFrame(this.draw);
     }
-    stopDraw(){
+    stopDraw() {
         cancelAnimationFrame(this.latestRequestAnimationId);
     }
 
