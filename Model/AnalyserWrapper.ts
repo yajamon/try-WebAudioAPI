2a76425ace4de53a17cb8dd70783a1b2d01c8b5b
yajamon <yajamon.tatsuki@gmail.com>
2016-12-05 12:13:41 +0900

    Add rendering logic

--- Model/AnalyserWrapper.ts
+++ Model/AnalyserWrapper.ts
@@ -15,7 +15,37 @@ class AnalyserWrapper {
     }
 
     draw() {
+        const WIDTH = this.canvas.width;
+        const HEIGHT = this.canvas.height;
         this.latestRequestAnimationId = requestAnimationFrame(this.draw);
+        this.analyser.getByteTimeDomainData(this.dataArray);
+        this.context.fillStyle = 'rgb(200, 200, 200)';
+        this.context.fillRect(0, 0, WIDTH, HEIGHT);
+
+        this.context.lineWidth = 2;
+        this.context.strokeStyle = 'rgb(0, 0, 0)';
+
+        this.context.beginPath();
+
+        const sliceWidth = WIDTH * 1.0 / this.bufferLength;
+        let x = 0;
+
+        for (let i = 0; i < this.bufferLength; i++) {
+
+            var v = this.dataArray[i] / 128.0;
+            var y = v * HEIGHT / 2;
+
+            if (i === 0) {
+                this.context.moveTo(x, y);
+            } else {
+                this.context.lineTo(x, y);
+            }
+
+            x += sliceWidth;
+        }
+
+        this.context.lineTo(WIDTH, HEIGHT / 2);
+        this.context.stroke();
     }
     stopDraw() {
         cancelAnimationFrame(this.latestRequestAnimationId);
